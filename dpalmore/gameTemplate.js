0036fb4d4ce6d0608b3de38202831a9530b4742c
dpalmore03 <77513921+dpalmore03@users.noreply.github.com>
2021-04-14 10:01:34 -0700

    Mob Eating Game
    The Game...

--- dpalmore/gameTemplate.js
+++ dpalmore/gameTemplate.js
@@ -1,10 +1,15 @@
 //sources
 // https://eloquentjavascript.net/code/chapter/17_canvas.js
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
-// Mouse function learned in class on 3/25/21
-// Color changing code provided by Mr. Cozort last calss (3/25/21)
-//Timer code provided by Mr. Cozort in class today (3/31/21)
-//******** ALL GLOBALS AND UTILITY FUNCTIONS *********
+//Alert pop-up from W3 Schools, originally "Press enter", changed to "Ready, Set Go"
+//Color change and Gravity codes provided by Mr. Cozort in class
+//Timer/Counter code provided by Mr. Cozort
+//Game over alert code provided by Mr. Cozort
+
+//RULES: Score as many points as possible by eating the black and yellow mobs. One point is awarded for each of black and orange squares that are eaten
+//RULES PT 2: Eat as manny mobs as possible in 60 seconds before the game ends. Refresh page to restart the game after 60 seconds.
+
+//##################### ALL GLOBALS AND UTILITY FUNCTIONS ###################
 
 //initializing GLOBAL variables to create a canvas
 let canvasDiv;
@@ -12,11 +17,13 @@ let canvas;
 let ctx;
 let WIDTH = 768;
 let HEIGHT= 768;
-let GRAVITY = 9.8;
 let SCORE = 0;
+let GRAVITY = 9.8;
 let paused = false;
 let timerThen = Math.floor(Date.now() / 1000);
 
+//
+let effects = [];
 
 //walls
 let walls = [];
@@ -24,6 +31,7 @@ let walls = [];
 //array for mobs/enemies
 let mobs1 = [];
 let mobs2 = [];
+let mobs3 = [];
 
 // lets us know if game is initialized
 let initialized = false;
@@ -38,28 +46,34 @@ let mousePos = {
   y: 0
 };
 
-let mouseClicks = {
-  x: 0,
-  y: 0
+let mouseClick = {
+  x: null,
+  y: null
 };
 
-let mouseClickX = 0;
-let mouseClickY = 0;
 
-function mouseCollide(obj) {
-  if (mouseClickX <= obj.x + obj.w &&
-    obj.x <= mouseClickX&&
-    mouseClickY <= obj.y + obj.h &&
-    obj.y <= mouseClickY
+function pointCollide(point, obj) {
+  if (point.x <= obj.x + obj.w &&
+    obj.x <= point.x &&
+    point.y <= obj.y + obj.h &&
+    obj.y <= point.y
   ) {
+    console.log('point collided');
     return true;
   }
 }
 
-//spawner
-function spawnMob(x, arr){
+function signum(){
+ let options = [-1,1];
+ index = Math.floor(Math.random()*options.length);
+ result = options[index];
+ return result;
+}
+
+//mob spawner
+function spawnMob(x, arr, color){
 for (i = 0; i < x; i++){
-  arr.push(new Mob(60,60, 200, 100 ('pink'), Math.random()*-2, Math.random()*-2));
+  arr.push(new Mob(60,60, 200, 100, color, Math.random()*3*signum(), Math.random()*3*signum()));
 }
 }
 // draws text on canvas
@@ -77,9 +91,6 @@ function countUp(end) {
   timerNow = Math.floor(Date.now() / 1000);
   currentTimer = timerNow - timerThen;
   if (currentTimer >= end){
-    if (mobs2.length < 10){
-    spawnMob(20, mobs2);
-  }
     return end;
   }
   return currentTimer;
@@ -142,29 +153,6 @@ function init() {
 }
 
 
-
-//########################### Initialize game function #######################
-
-function init() {
-  // create a new div element
-  canvasDiv = document.createElement("div");
-  canvasDiv.id = "chuck";
-  // and give it some content
-  canvas = document.createElement('canvas');
-  // add the text node to the newly created div
-  canvasDiv.appendChild(canvas);
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(canvasDiv, currentDiv);
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-  document.getElementById("chuck").style.width = canvas.width + 'px';
-  document.getElementById("chuck").style.height = canvas.height + 'px';
-  ctx = canvas.getContext('2d');
-  initialized = true;
-}
-
-
 //############################ ALL GAME CLASSES #########################
 class Sprite {
   constructor(w, h, x, y, c) {
@@ -175,6 +163,8 @@ class Sprite {
     this.color = c;
     this.spliced = false;
     }
+    get cx() { return this.x + this.w * 0.5; }
+    get cy() { return this.y + this.h * 0.5; }
     inbounds(){
       if (this.x + this.w < WIDTH &&
           this.x > 0 &&
@@ -186,6 +176,42 @@ class Sprite {
       else{
         return false;
       }
+    }
+    //De
+    //source for collision: https://pothonprogramming.github.io/
+    collideRectangle(rect) {
+
+      var dx = rect.cx - this.cx;// x difference between centers
+      var dy = rect.cy - this.cy;// y difference between centers
+      var aw = (rect.w + this.w) * 0.5;// average width
+      var ah = (rect.h + this.h) * 0.5;// average height
+
+      /* If either distance is greater than the average dimension there is no collision. */
+      if (Math.abs(dx) > aw || Math.abs(dy) > ah) return false;
+
+      /* To determine which region of this rectangle the rect's center
+      point is in, we have to account for the scale of the this rectangle.
+      To do that, we divide dx and dy by it's width and height respectively. */
+      if (Math.abs(dx / this.w) > Math.abs(dy / this.h)) {
+      
+
+        if (dx < 0) {rect.x = this.x - rect.w;
+          ctx.fillStyle = 'green';
+          ctx.fillRect(0, 0, WIDTH/0, HEIGHT/0);
+          ctx.strokeRect(0, 0, WIDTH/0, HEIGHT/0);
+        }// left
+        else rect.x = this.x + this.w; // right
+        
+
+      } else {
+
+        if (dy < 0) rect.y = this.y - rect.h; // top
+        else rect.y = this.y + this.h; // bottom
+
+      }
+
+      return true;
+
     }
     collide(obj) {
       if (this.x <= obj.x + obj.w &&
@@ -208,18 +234,26 @@ class Player extends Sprite {
   }
   moveinput() {
     if ('w' in keysDown || 'W' in keysDown) { // Player control
-        this.vx = 0;
+        this.dx = 0;
+        this.dy = -1;  
+        // this.vx = 0;
         this.vy = -this.speed;
     } else if ('s' in keysDown || 'S' in keysDown) { // Player control
-        this.vx = 0;
+        this.dx = 0;
+        this.dy = 1;  
+        // this.vx = 0;
         this.vy = this.speed;
 
     } else if ('a' in keysDown || 'A' in keysDown) { // Player control
-        this.vy = 0;
+        this.dx = -1;
+        this.dy = 0;
+        // this.vy = 0;
         this.vx = -this.speed;
 
     } else if ('d' in keysDown || 'D' in keysDown) { // Player control
-        this.vy = 0;
+        this.dx = 1;
+        this.dy = 0;
+        // this.vy = 0;
         this.vx = this.speed;
     } else if ('e' in keysDown || 'E' in keysDown) { // Player control
       this.w += 1;
@@ -234,8 +268,10 @@ class Player extends Sprite {
       
   }
     else{
+      // this.dx = 0;
+      // this.dy = 0;
       this.vx = 0;
-      this.vy = 0;
+      this.vy = GRAVITY;
     }
 }
   update(){
@@ -285,10 +321,11 @@ class Mob extends Sprite {
         // alert('out of bounds');
         // console.log('out of bounds');
       }
-      if (mouseCollide(this)){
-        // alert("direct hit!!!");
+      if (pointCollide(mouseClick, this)){
+        console.log("direct hit!!!");
         // how do I tell it to be spliced???
         this.spliced = true;
+        // this.vx *= -1;
       }
     }
     draw() {
@@ -310,18 +347,35 @@ class Wall extends Sprite {
     }
 }
 
+class Effect extends Sprite {
+  constructor(w, h, x, y, c) {
+    super(w, h, x, y, c);
+    this.type = "normal";
+    }
+    draw() {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+      ctx.strokeRect(this.x, this.y, this.w, this.h);
+    }
+    update(){
+      this.w+=2;
+      this.h+=2;
+      this.x-=1;
+      this.y-=1;
+      setTimeout(() => this.spliced = true, 250)
+    }
+}
+
 // ###################### INSTANTIATE CLASSES ##########################
 let player = new Player(25, 25, WIDTH/2, HEIGHT/2, 'red', 0, 0);
 
 // adds two different sets of mobs to the mobs array
-for (i = 0; i < 10; i++){
-  mobs1.push(new Mob(60,60, 200, 10, 'pink', Math.random()*-2, Math.random()*-2));
-}
-while (mobs2.length < 20){
-  mobs2.push(new Mob(10,10, 250, 200, 'purple', Math.random()*-2, Math.random()*-2));
-}
+spawnMob(20, mobs1, 'black');
+spawnMob(20, mobs2, 'yellow');
+
+
 while (walls.length < 20){
-  walls.push(new Wall(400,10, Math.floor(Math.random()*500), Math.floor(Math.random()*1000), 'orange'));
+  walls.push(new Wall(200,15, Math.floor(Math.random()*500), Math.floor(Math.random()*1000), 'green'));
 }
 
 // ########################## USER INPUT ###############################
@@ -340,6 +394,7 @@ addEventListener("keyup", function (e) {
 addEventListener('mousemove', function (e) {
   mouseX = e.offsetX;
   mouseY = e.offsetY;
+  
   // we're gonna use this
   mousePos = {
     x: mouseX,
@@ -349,24 +404,59 @@ addEventListener('mousemove', function (e) {
 
 // gets mouse position when clicked
 addEventListener('mousedown', function (e) {
-  console.log(`Screen X/Y: ${e.screenX}, ${e.screenY}, Client X/Y: ${e.clientX}, ${e.clientY}`);
-  mouseClickX = e.clientX;
-  mouseClickY = e.clientY;
-  mouseClicks = {
-    x: mouseClickX,
-    y: mouseClickY
-  };
+  // console.log(`Screen X/Y: ${e.screenX}, ${e.screenY}, Client X/Y: ${e.clientX}, ${e.clientY}`);
+  mouseClick.x = e.offsetX;
+  mouseClick.y = e.offsetY;
+  effects.push(new Effect(15, 15, mouseClick.x - 7, mouseClick.y - 7, 'green'))
 });
 
 addEventListener('mouseup', function() {
-  mouseClickX = null;
-  mouseClickY = null;
+  setTimeout(()=>{
+    mouseClick.x = null;
+    mouseClick.y = null;
+  },
+    1000
+  )
+  
 });
 
+let GAMETIME = null;
 // ###################### UPDATE ALL ELEMENTS ON CANVAS ################################
 function update() {
   player.update();
+  for (e of effects) {
+    e.update();
+  }
+  for (e in effects){
+    if (effects[e].spliced){
+      effects.splice(e, 1);
+    }
+  }
+  GAMETIME = counter();
+  if (GAMETIME > 60){
+   alert("game over...");
+   }
   //updates all mobs in a group
+  for (let w of walls){
+   
+    // if (player.collide(w) && player.dy == 1){
+    //   player.dx = 0;
+    //   player.vy*=-1;
+    //   player.y = w.y-player.h;
+    // }
+    // if (player.collide(w) && player.dy == -1){
+    //   player.vy*=-1;
+    //   player.y = w.y + w.h;
+    // }
+    // if (player.collide(w) && player.dx == 1){
+    //   player.vx*=-1;
+    //   player.x = w.x-player.w;
+    // }
+    // if (player.collide(w) && player.dx == -1){
+    //   player.vx*=-1;
+    //   player.x = w.x + w.w;
+    // }
+  }
   for (let m of mobs1){
     m.update();
     if (player.collide(m)){
@@ -377,16 +467,17 @@ function update() {
   for (let m of mobs2){
     m.update();
     if (player.collide(m)){
-      SCORE++;
       m.spliced = true;
     }
   }
-   for (let m1 of mobs1) {
-     for (let m2 of mobs2){
-       if (m1.collide(m2)){
-        //  console.log('two boxes collided');
-        m1.color = 'blue';
-        m2.color = 'green';
+   for (let m2 of mobs2) {
+     for (let m1 of mobs1){
+       if (m2.collide(m1)){
+        m1.vx *= 1;
+        m1.vy *= 1;
+        m2.vx *= 1;
+        m2.vy *= 1;
+        // m2.spliced = true;
        }
      }
    }
@@ -396,18 +487,26 @@ function update() {
       mobs1.splice(m, 1);
     }
   }
+  for (let m in mobs2){
+    if (mobs2[m].spliced){
+      mobs2.splice(m, 1);
+    }
+  }
+  if (mobs1.length < 1){
+    spawnMob(30, mobs1);
+  }
 }
 
 // ########## DRAW ALL ELEMENTS ON CANVAS ##########
 function draw() {
   // clears the canvas before drawing
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawText('black', "24px Helvetica", "left", "top", "Timer: " + countUp(25), 500, 0);
+  drawText('black', "24px Helvetica", "left", "top", "Timer: " + GAMETIME, 500, 0);
   drawText('black', "24px Helvetica", "left", "top", "Score: " + SCORE, 600, 0);
   drawText('black', "24px Helvetica", "left", "top", "FPS: " + fps, 400, 0);
   drawText('black', "24px Helvetica", "left", "top", "Delta: " + gDelta, 400, 32);
   drawText('black', "24px Helvetica", "left", "top", "mousepos: " + mouseX + " " + mouseY, 0, 0);
-  drawText('black', "24px Helvetica", "left", "top", "mouseclick: " + mouseClickX + " " + mouseClickY, 0, 32);
+  drawText('black', "24px Helvetica", "left", "top", "mouseclick: " + mouseClick.x + " " + mouseClick.y, 0, 32);
   player.draw();
 
   for (let w of walls){
@@ -419,8 +518,10 @@ function draw() {
   for (let m of mobs2){
     m.draw();
   }
+  for (let e of effects){
+    e.draw();
+  }
 }
-
 
 // set variables necessary for game loop
 let fps;
@@ -444,7 +545,9 @@ function main() {
   then = now;
   requestAnimationFrame(main);
 }
-//performance.now measures time in milliseconds
-//if (initialized) { update();to draw(); is the animation itself
-//Math.ceil gets the fps to the nearest whole number during calculations
-//delta=now-then determines time since last frame
+
+if (confirm("Ready?, Set?, Go!!!")) {
+  txt = "You pressed Ok!";
+} else {
+  txt = "You pressed Cancel!";
+}
